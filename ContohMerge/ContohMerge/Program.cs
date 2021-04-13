bc0009b1ac2665d227e44d4025e4e1e41910a9a8
Alfahmist <48145002+alfahmist@users.noreply.github.com>
2021-04-13 13:16:14 +0700

    First Commit

---
+++ ContohMerge/ContohMerge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContohMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
